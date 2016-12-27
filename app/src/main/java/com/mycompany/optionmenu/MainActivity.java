85685cf38808cc930ec643957d563f18225a776c
force1 <arvind.dash555@gmail.com>
2016-12-26 22:12:26 -0800

    optionmenu

---
+++ app/src/main/java/com/mycompany/optionmenu/MainActivity.java
@@ -0,0 +1,43 @@
+package com.mycompany.optionmenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Toast.makeText(getApplicationContext(), "RED Selected", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(getApplicationContext(), "GREEN Selected", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.item3:
+                Toast.makeText(getApplicationContext(), "BLUE Selected", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
